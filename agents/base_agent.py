af77e5ada14e7aad1040e5dde1afd468e42807e4
only1allan <kariukiallan850@gmail.com>
2025-11-26 01:26:44 +0300

    Add base agent class with common functionality

---
+++ agents/base_agent.py
@@ -0,0 +1,52 @@
+"""
+Abstract base class for all specialized agents
+"""
+
+from semantic_kernel import Kernel
+#TODO:Refactor to use Kernel Factory
+from semantic_kernel.agents import ChatCompletionAgent
+from abc import ABC, abstractmethod
+
+
+class BaseMarketingAgent(ABC):
+    """Base class for all marketing agents with custom functionality"""
+
+    def __init__(self, kernel: Kernel, config: dict):
+        self.kernel = kernel
+        self.config = config
+        self.agent_config = config["agents"][self.agent_name]
+
+    
+    @property
+    @abstractmethod
+    def agent_name(self) -> str:
+        """Return the agent's unique identifer"""
+        pass
+
+    @property
+    @abstractmethod
+    def instructions(self) -> str:
+        """Return the agent's system instructions"""
+        pass
+
+    @abstractmethod
+    def get_plugins(self) -> list:
+        """Return list of plugins this agent uses."""
+        pass
+
+    def create(self) -> ChatCompletionAgent:
+        """Create and configure the ChatCompletionAgent"""
+
+        agent = ChatCompletionAgent(
+            service_id="default",
+            kernel=self.Kernel,
+            name=self.agent_name,
+            instructions=self.instructions
+        )
+
+        #add agent specific plugins
+
+        for plugin in self.get_plugins():
+            self.kernel.add_plugin(plugin)
+
+        return agent
